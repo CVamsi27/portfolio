0520228ef66892ba5f074306d9be697940c0b8e7
Vamsi Krishna Chandaluri <55316723+CVamsi27@users.noreply.github.com>
2024-12-02 19:40:58 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,15 +4,15 @@ const Footer = () => {
   const d = new Date();
   let year = d.getFullYear();
   return (
-    <footer className="flex justify-center w-full px-4 py-3 sticky bottom-0 border-t-1 bg-card">
-      <div className="flex flex-col lg:flex-row gap-2 items-center">
+    <footer className="bottom-0 w-full flex flex-col md:flex-row justify-center gap-2 md:justify-center border-t-1 p-2">
+      <div className="flex flex-row gap-2 items-center justify-center h-full">
         <div className="flex items-center gap-2">
-          <Copyright />
-          <span className="text-base">{year}, Built with</span>
+          <Copyright className="hover:text-primary-500" />
+          <span className="text-sm sm:text-base">{year}, Built with</span>
         </div>
         <div className="flex gap-2 items-center">
-          <Heart className="text-red-500 hover:bg-red-500" />
-          <span className="text-base">by Vamsi Krishna</span>
+          <Heart className="text-red-500 hover:text-black" />
+          <span className="text-sm sm:text-base">by Vamsi Krishna</span>
         </div>
       </div>
     </footer>
